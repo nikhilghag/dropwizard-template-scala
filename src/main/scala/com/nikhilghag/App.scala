4b450f96539311e7bd50b9f0252f69eac6619d37
Nikhil Ghag <nikhil.g.ghag@gmail.com>
2018-02-04 13:31:34 +0530

    Create helloworld application class

--- src/main/scala/com/nikhilghag/App.scala
+++ src/main/scala/com/nikhilghag/App.scala
@@ -1,9 +1,16 @@
 package com.nikhilghag
 
-/**
- * Hello world!
- *
- */
-object App extends Application {
-  println( "Hello World!" )
+import com.datasift.dropwizard.scala.ScalaApplication
+import com.nikhilghag.resources.HelloWorldResources
+import io.dropwizard.setup.{Bootstrap, Environment}
+
+
+object App extends ScalaApplication[HelloWorldConfiguration] {
+  override def init(bootstrap: Bootstrap[HelloWorldConfiguration]) = {
+  }
+
+  override def run(configuration: HelloWorldConfiguration, environment: Environment): Unit = {
+    val helloWorldResources = new HelloWorldResources(configuration.template, configuration.defaultName)
+    environment.jersey().register(helloWorldResources)
+  }
 }
